f150e83a0858dad554a81877a6bde98f12e268a4
JesusCoboArrogante <jesusoscar.cobo@alumno.iesluisvives.org>
2024-02-24 10:26:13 +0100

    first commit

--- empresa/ejercicio03/jesus.cobo_empresa_ejercicio03.sql
+++ empresa/ejercicio03/jesus.cobo_empresa_ejercicio03.sql
@@ -1,10 +1,11 @@
 use examen;
-/*ejercicio01*/ 
 
+/*ejercicio01*/ 
 SELECT nomde, presu+presu/12*0.1*3  as "presupuesto medio mensual"
 from tdepto
 where (presu/12) * 9 > 5.0
 order by nomde asc;
+
 
 /*ejercicio02*/
 select salar * 0.2, salar, comis
@@ -38,10 +39,21 @@ order by char_length(nomem) asc, nomem asc;
 /*ejercicio07*/
 select nomem, salar
 from temple
-where year(fecin) >= 1988
+where year(fecin) >= 1988;
 
 /*ejercicio08*/
 
 
 
-select * from temple
+/*ejercicio09*/
+select avg(year(now()) - year(fecin)) as "año de servicio"
+from temple
+where numde = 111 or numde = 112;
+
+/*ejercicio10*/
+select max(salar) as maximo, min(salar) as minimo, avg(salar) as medio
+from temple
+group by numde 
+having  avg(year(fecin)) > 1948;
+
+
