6e7cf38ccb879ed7f5c26d4c6c645bfdffae44f7
Jesus Cobo <jessucobo1997@gmail.com>
2024-05-25 11:25:01 +0200

    first commit

--- fin_curso/acciones.sql
+++ fin_curso/acciones.sql
@@ -3,9 +3,42 @@ DROP PROCEDURE IF EXISTS horario;
 delimiter //
 create procedure horario (in id_empleado int)
 begin
-select max(fecha), id_tienda from cuadrante 
-where id_empleado = id_empleado;
+select empleado.nombre,horario.entrada, horario.salida, horarioempleado.max(fecha) from horarioempleado 
+inner join horarioempleado
+on  horaioempleado.id_empleado = empleado.id_empleado
+inner join horarioempleado
+on  horarioempleado.id_horario = horario.id_horario  
+where id_empleado = horarioempleado.id_horario;
+end // 
+call horario(1);
+
+describe empleadohorario
+
+select*from horario
+
+
+/*menor de edad*/
+delimiter //
+create trigger menores
+before insert on alquiler
+for each row
+begin
+	declare edad_cliente int;
+	declare edad_pelicula int;
+    
+select edad into edad_cliente from cliente where id_cliente = new.id_cliente;
+select edad into edad_pelicula from pelicula where id_pelicula = new.id_pelicula;
+
+if edad_cliente < edad_pelicula then 
+call mensaje_01();
+end if;
+end //
+delimiter ;
+
+delimiter //
+create procedure mensaje_01()
+begin
+select "no se le puede dar el producto al cliente" as mensaje_01;
 end//
-call horario(1)
-
-
+delimiter ;
+select * from cliente
--- fin_curso/tablas.sql
+++ fin_curso/tablas.sql
@@ -1,102 +1,139 @@
+drop database netflixclub;
 create database netflixclub;
 use netflixclub;
-create table empleado(
-id_empleado  int auto_increment primary key,
-nombre varchar (10) ,
-apellido varchar (10),
-telefono int (9) unique
-);
 create table tienda(
-id_tienda int auto_increment primary key,
+id_tienda int (2) auto_increment primary key,
 localidad varchar (10),
 calle varchar (20),
 telefono int (9) unique
 );
 
-create table cuadrante(
-id_empleado int,
-fecha date,
-id_tienda int,
-primary key(id_empleado,fecha),
-foreign key (id_empleado) references empleado(id_empleado)
-on delete cascade 
-on update cascade,
+create table empleado(
+id_empleado int (2)  auto_increment primary key,
+nombre varchar (10) ,
+apellido varchar (10),
+telefono int (9) unique,
+id_tienda int (2),
 foreign key (id_tienda) references tienda(id_tienda)
-on delete cascade 
+on update cascade
+on delete cascade
+);
+
+create table horario(
+id_horario int (1) auto_increment primary key,
+turno varchar (6),
+entrada varchar (5),
+salida varchar (5)
+);
+
+create table empleadoHorario(
+id_empleado int (2),
+id_horario int (1),
+fecha date, 
+primary key (id_empleado, fecha),
+foreign key (id_empleado) references empleado (id_empleado)
+on delete cascade
+on update cascade,
+foreign key (id_horario) references horario (id_horario)
 on update cascade
 );
 
 create table pelicula (
-id_pelicula int auto_increment primary key,
+id_pelicula int (2) auto_increment primary key,
 nombre varchar (100),
 anyo int (4),
 edad int (3)
 );
+
 create table genero (
 id_genero varchar (30) primary key
 );
-alter table genero add constraint id_genero check(id_genero in ('accion', 'aventura', 'ciencia ficcion', 'comedia', 'documentales', 'drama', 'fantasia', 'musicales', 'suspense', 'terror'));
+
 
 create table generoPelicula(
 id_genero varchar (30),
-id_pelicula int,
+id_pelicula int (2),
 primary key (id_pelicula, id_genero),
 foreign key (id_genero) references genero(id_genero)
-on delete cascade on update cascade,
+on update cascade
+on delete cascade,
 foreign key (id_pelicula) references pelicula(id_pelicula)
-on delete cascade 
 on update cascade
+on delete cascade
 );
 
-create table tipoPelicula(
-id_tipopelicula int auto_increment primary key,
-id_pelicula int,
-num_peliculas int (2),
-peliculas_alquiladas int (2) default 0,
-tipo varchar (10),
-id_tienda int,
-foreign key (id_tienda) references tienda(id_tienda)
-on delete cascade 
-on update cascade,
-foreign key (id_pelicula) references pelicula (id_pelicula)
-on delete cascade 
-on update cascade
-
+create table stock(
+id_stock int (2) auto_increment primary key,
+id_pelicula int (2),
+total int (2)
 );
+
+
+
 
 create table cliente(
-id_cliente int auto_increment primary key,
+id_cliente int (2) auto_increment primary key,
 nombre varchar(10),
 apellido varchar(10),
 numero_telefono int (9),
 edad int,
-anyo_nacimiento year
+anyo_nacimiento year,
+check (edad >= 15)
 );
+
+
 
 create table alquiler(
-id_alquiler int auto_increment primary key,
-id_cliente int,
-id_tipopelicula int,
+id_alquiler int (2) auto_increment primary key,
+id_cliente int (2),
+id_stock int (2),
 fx_adquisicion date,
 fx_devolucion date,
-id_tienda int,
-cantidad int (2),
+cantidad int (2) default 1,
 devuelta int (2) default 0,
-foreign key (id_tienda) references tienda(id_tienda)
-on delete cascade 
-on update cascade,
-foreign key (id_cliente) references cliente(id_cliente)
-on delete cascade 
-on update cascade,
-foreign key (id_tipopelicula) references tipopelicula (id_tipopelicula)
-on delete cascade 
+foreign key (id_cliente) references cliente (id_cliente)
 on update cascade
-);
-create table retrasos(
-id_cliente int,
-id_alquiler int,
-dias int,
-debido int,
-primary key (id_cliente,id_alquiler)
+on delete cascade,
+foreign key (id_stock) references stock (id_stock)
+on update cascade
+on delete cascade
 );
 
+create table stockAlquiler(
+id_alquiler int (2),
+id_stock int (2),
+primary key(id_alquiler,id_stock),
+foreign key (id_alquiler) references alquiler(id_alquiler)
+on update cascade
+on delete cascade,
+foreign key (id_stock) references stock (id_stock)
+on update cascade
+on delete cascade
+); 
+
+
+create table retrasos(
+id_retraso int (2) primary key auto_increment,
+id_alquiler int (2),
+dias int,
+debido double (5,2) default 0.20,
+foreign key (id_alquiler) references alquiler (id_alquiler)
+);
+
+create table tiendastock (
+id_tienda int,
+id_stock int,
+num_peliculas int,
+peliculas_alquiladas int default 0,
+primary key (id_tienda, id_stock),
+foreign key (id_tienda) references tienda(id_tienda),
+foreign key (id_stock) references stock(id_stock)
+);
+
+create table tiendaalquiler (
+id_tienda int,
+id_alquiler int,
+primary key (id_tienda,id_alquiler),
+foreign key (id_tienda) references tienda (id_tienda),
+foreign key (id_alquiler) references alquiler (id_alquiler)
+)
